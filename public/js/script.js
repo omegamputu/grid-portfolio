b3afa5ea2e1743d672dc92131f2f665b8d4a373b
Omega Mputu <kutekemeni93@gmail.com>
2022-03-04 18:41:46 +0100

    Affichage dynamique des réalisation

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,24 @@
-$(function () {
-    
-})();
+$(document).ready(function () {
+    var $active = false;
+
+    $('.work').click(function (e) {
+        e.preventDefault();
+
+        var $work = $(this);
+        var $detail = $work.parent().parent().nextAll('.row-detail:first');
+        var $work_detail = $('.work_detail', $work).clone();
+        //$work.find('.work_detail');
+
+        if($active) {
+            var $el = $active;
+
+            $el.slideUp(500, function () {
+                $el.remove();
+            })
+        }
+
+        $detail.append($work_detail);
+        $work_detail.slideDown();
+        $active = $work_detail.removeClass("d-none");
+    });
+}); 
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'js')
 .sass('resources/sass/app.scss', 'css')
-.setPublicPath('public').browserSync();
+.setPublicPath('public').browserSync(); 
